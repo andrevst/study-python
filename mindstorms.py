5bafafef4ba6bff681444b1f155126f4782bf691
Andre Tonelli <aveigas@br.ibm.com>
2017-12-16 10:51:15 -0200

    Draw square

---
+++ mindstorms.py
@@ -0,0 +1,15 @@
+import turtle
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("black")
+    brad = turtle.Turtle()
+    brad.forward(100)
+    brad.right(45)
+    brad.forward(100)
+    brad.right(45)
+    brad.forward(100)
+    brad.right(45)
+    brad.forward(100)
+    brad.right(45)
+    window.exitonclick()
+draw_square()
