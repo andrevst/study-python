947981819ca43612bfea2f2b36f8620f3fed8085
Andre Tonelli <aveigas@br.ibm.com>
2017-12-16 10:51:38 -0200

    Wait for it

--- mindstorms.py
+++ mindstorms.py
@@ -1,8 +1,15 @@
 import turtle
+import time
+
 def draw_square():
     window = turtle.Screen()
     window.bgcolor("black")
+
+    time.sleep(2)
+
     brad = turtle.Turtle()
+    brad.color("green")
+    brad.speed(2)
     brad.forward(100)
     brad.right(45)
     brad.forward(100)
