4593c2c8698c4dcdb426c62f51918e438156d8be
Andre Tonelli <aveigas@br.ibm.com>
2017-12-16 10:52:19 -0200

    Have you meet Jon?

--- mindstorms.py
+++ mindstorms.py
@@ -26,5 +26,13 @@ def draw_square():
     brad.right(45)
     brad.forward(100)
     brad.right(45)
+
+    jon = turtle.Turtle()
+    jon.color("pink")
+    jon.shape("arrow")
+
+    jon.circle(100)
+
     window.exitonclick()
+
 draw_square()
