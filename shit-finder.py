f7789f8ecc2f1c4ca1ce86feaa64bf2da11b8396
Andre Tonelli <aveigas@br.ibm.com>
2017-12-25 12:00:09 -0200

    Removing unnecessary print

--- shit-finder.py
+++ shit-finder.py
@@ -4,14 +4,12 @@ def read_text():
 
     quotes = open("/Users/andretonelli/projects/fullstackUdacity/study-python/shit_text.txt")
     content = quotes.read()
-    print(content)
     quotes.close()
     check_shit(content)
 
 def check_shit(content):
     connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + content)
     output = connection.read()
-    print(output)
     connection.close()
     if "true" in output:
         print("Idiot, you wrote a shit.")
