f38e1e36b5b3db841e731fc0f9ae2d7561de20f8
Andre Tonelli <aveigas@br.ibm.com>
2017-12-25 11:58:54 -0200

    Check if it is a bad word with the link

--- shit-finder.py
+++ shit-finder.py
@@ -1,8 +1,17 @@
+import urllib
+
 def read_text():
 
     quotes = open("/Users/andretonelli/projects/fullstackUdacity/study-python/shit_text.txt")
     content = quotes.read()
     print(content)
     quotes.close()
+    check_shit(content)
+
+def check_shit(content):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + content)
+    output = connection.read()
+    print(output)
+    connection.close()
 
 read_text()
