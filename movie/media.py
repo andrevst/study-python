005d0b4e1d144b4047240dde573aea1b121e491e
Andre Tonelli <aveigas@br.ibm.com>
2017-12-26 13:34:27 -0200

    Adding movie instances

--- movie/media.py
+++ movie/media.py
@@ -1,1 +1,6 @@
 class Movie():
+    def __init__ (self,title,sinopse,poster_img_url,trailer_youtube_url):
+        self.title = title
+        self.sinopse = sinopse
+        self.poster_img_url = poster_img_url
+        self.trailer_youtube_url = trailer_youtube_url
