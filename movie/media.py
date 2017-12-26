43a59d7990e658ccad4e5e4c0da764058b1fb8f5
Andre Tonelli <aveigas@br.ibm.com>
2017-12-26 17:54:24 -0200

    Adding show trailer function to Movie class

--- movie/media.py
+++ movie/media.py
@@ -1,6 +1,12 @@
+import webbrowser
+
 class Movie():
-    def __init__ (self,title,sinopse,poster_img_url,trailer_youtube_url):
+    def __init__ (self,title,year,sinopse,poster_img_url,trailer_youtube_url):
         self.title = title
+        self.year = year
         self.sinopse = sinopse
         self.poster_img_url = poster_img_url
         self.trailer_youtube_url = trailer_youtube_url
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
