4b42102a1b138266d560f20e8cb6b4d0dbbb24d2
Andre Tonelli <aveigas@br.ibm.com>
2018-01-05 16:06:33 -0200

    Parent Class

---
+++ heir.py
@@ -0,0 +1,7 @@
+class Parent():
+    """ This class use a parent to explain a parent class. """
+
+    def __init__(self, last_name, eye_color):
+        self.last_name = last_name
+        self.eye_color = eye_color
+
