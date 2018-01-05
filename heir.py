cb6ca7850dc5ba38c5d08d76c626f5ec197f571e
Andre Tonelli <aveigas@br.ibm.com>
2018-01-05 16:07:00 -0200

    Child Class

--- heir.py
+++ heir.py
@@ -10,5 +10,15 @@ class Parent():
         print("Last Name: "+ self.last_name)
         print("Eye Color: "+ self.eye_color)
 
+class Child(Parent):
+    """ This class create child of the Parent class. """
+
+    def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child constructor called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
+
 marshall_law = Parent("Law", "Brown")
 marshall_law.show_info()
+forrest_law = Child("Law", "Brown", 5)
+forrest_law.show_info()
