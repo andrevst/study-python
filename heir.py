d764bb0ddd733e9f2148f38c90b85ace4229534a
Andre Tonelli <aveigas@br.ibm.com>
2018-01-05 16:06:51 -0200

    Showing Parent

--- heir.py
+++ heir.py
@@ -2,6 +2,13 @@ class Parent():
     """ This class use a parent to explain a parent class. """
 
     def __init__(self, last_name, eye_color):
+        print("Parent constructor called")
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print("Last Name: "+ self.last_name)
+        print("Eye Color: "+ self.eye_color)
+
+marshall_law = Parent("Law", "Brown")
+marshall_law.show_info()
