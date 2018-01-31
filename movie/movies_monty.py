e27aefa79778080b916b230d6dc38b15c4f8b551
Andre Tonelli <aveigas@br.ibm.com>
2018-01-31 17:28:29 -0200

    Fixing test

--- movie/movies_monty.py
+++ movie/movies_monty.py
@@ -1,7 +1,6 @@
 import fresh_tomatoes
 import media
 
-the_holy_grail = media.Movie("Monty Python and The Holy Grail","1975", "In 932 AD, King Arthur and his squire, Patsy, travel throughout Britain searching for men to join the Knights of the Round Table. In Arthur's travels, he recruits Sir Bedevere the Wise, Sir Lancelot the Brave, Sir Galahad the Pure, and Sir Robin the Not-Quite-So-Brave-as-Sir-Lancelot, along with their squires and Robin's troubadours. Arthur leads the men to Camelot, but decide it is a silly place and set off elsewhere. As they turn away, God speaks to them and instruct Arthur to find the Holy Grail.", "https://upload.wikimedia.org/wikipedia/en/0/08/Monty-Python-1975-poster.png", "https://youtu.be/urRkGvhXc8w")
 the_holy_grail = media.Movie("Monty Python and The Holy Grail","1975",
                              "932 AD, King Arthur travel throughout Britain"
                              " searching the Knights of the Round Table. "
